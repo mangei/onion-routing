4a6946582a700de7b621579cdcd99ed7bdb5dd20
Mihai Lepadat <mihailepadat@gmail.com>
2014-11-23 14:51:54 +0100

    node heartbeat

--- node/app/util/Global.java
+++ node/app/util/Global.java
@@ -1,6 +1,7 @@
 package util;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import handlers.KeyHandler;
 import model.RegisterRequest;
 import play.Application;
@@ -12,13 +13,19 @@ import play.libs.Json;
 import play.libs.ws.WS;
 import play.libs.ws.WSResponse;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * @author Mihai Lepadat
  *         Date: 11/18/14
  */
 public class Global extends GlobalSettings {
 
-    private static long REQUEST_WAITING_TIME = 10000;
+    private final static long REQUEST_WAITING_TIME = 10000;
+    private final static String ADDRESS_DIRECTORY_NODE = "127.0.0.1";
+    private final static String PORT_DIRECTORY_NODE = "9001";
+    private final static int HEARTBEAT_PERIOD = 5000;
 
     private static KeyHandler keyHandler;
 
@@ -28,15 +35,17 @@ public class Global extends GlobalSettings {
 
         keyHandler = new KeyHandler();
 
-        registerNode();
+        final String secret = registerNode();
+
+        startHeartbeat(secret);
     }
 
-    private void registerNode() {
+    private String registerNode() {
         String pubKey = keyHandler.getPublicKey();
         RegisterRequest registerRequest = buildRegisterRequest(pubKey);
 
         JsonNode json = Json.toJson(registerRequest);
-        F.Promise<String> promise = WS.url(getUri("localhost", "9001"))
+        F.Promise<String> promise = WS.url(getUri(ADDRESS_DIRECTORY_NODE, PORT_DIRECTORY_NODE, "register"))
                 .setContentType("application/json")
                 .post(json)
                 .map(new F.Function<WSResponse, String>() {
@@ -48,6 +57,28 @@ public class Global extends GlobalSettings {
 
         String result = promise.get(REQUEST_WAITING_TIME);
         Logger.debug("Registration result: " + result);
+
+        return result;
+    }
+
+    private void startHeartbeat(final String secret) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Logger.debug("Sending heartbeat...");
+                F.Promise<String> promise = WS.url(getUri(ADDRESS_DIRECTORY_NODE, PORT_DIRECTORY_NODE, "heartbeat"))
+                        .setContentType("application/json")
+                        .put(JsonUtility.convertToJson(secret))
+                        .map(new F.Function<WSResponse, String>() {
+                            @Override
+                            public String apply(WSResponse wsResponse) throws Throwable {
+                                return wsResponse.getBody();
+                            }
+                        });
+                Logger.debug("Heartbeat response: " + promise.get(REQUEST_WAITING_TIME));
+            }
+        }, HEARTBEAT_PERIOD, HEARTBEAT_PERIOD);
     }
 
     @Override
@@ -55,8 +86,8 @@ public class Global extends GlobalSettings {
         Logger.info("Application shutdown");
     }
 
-    private static String getUri(String ip, String port) {
-        return "http://" + ip + ":" + port +"/register";
+    private static String getUri(String ip, String port, String path) {
+        return "http://" + ip + ":" + port +"/" + path;
     }
 
     private RegisterRequest buildRegisterRequest(String pubKey) {
