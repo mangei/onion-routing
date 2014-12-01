071cef5cf5f257b9d62caa8c97a9e45024de6718
Thomas Rieder <thomas@rieder.io>
2014-12-02 00:32:17 +0100

    added landing page with bootstrap to quote service

--- quote/app/controllers/Application.scala
+++ quote/app/controllers/Application.scala
@@ -3,11 +3,12 @@ package controllers
 import models.Quote
 import play.api.mvc.{Controller, Action}
 import play.api.libs.json._
+import views.html.index
 
 object Application extends Controller {
 
-  val index = Action { request =>
-    Ok("Quote says hello!")
+  val getIndex = Action { request =>
+    Ok(index.render())
   }
 
   val getAQuote = Action { request =>
