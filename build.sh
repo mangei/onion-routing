1625d092f70414bc82a1fbb10d56afea247dde41
Thomas Rieder <thomas@rieder.io>
2014-12-02 01:00:32 +0100

    added build-script for all four components

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "THIS SCRIPT IS NOT PATH AGNOSTIC!"
+
+rm -rf build
+mkdir build
+
+for i in directory originator node quote
+do
+        echo "Building $i..."
+        cd $i
+        activator dist
+        mv target/universal/*.zip ../build
+        activator clean
+        cd ..
+        echo "...done"
+done
